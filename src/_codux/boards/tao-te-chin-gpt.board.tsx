90b2426f44a762ef41e2200ec3b09ff866c713c8
dan-mcateer <daniel.mcateer@udemy.com>
2023-04-21 14:51:48 -0400

    Change board name

--- src/_codux/boards/tao-te-chin-gpt.board.tsx
+++ src/_codux/boards/tao-te-chin-gpt.board.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 import { createBoard } from '@wixc3/react-board';
 
 export default createBoard({
-    name: 'taotechingpt board',
+    name: 'TaoTeChinGPT',
     Board: () => <div></div>
 });
