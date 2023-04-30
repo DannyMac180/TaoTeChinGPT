9c24bb617725fe5eea95c312af6faa21d68526b1
Danny McAteer <mcateerd2@gmail.com>
2023-04-30 02:48:26 +0000

    Create user document with Firebase cloud function on first auth

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,23 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const db = admin.firestore();
+
+export const createUserRecord = functions.auth.user().onCreate((user) => {
+    return db.collection('users').doc(user.uid).set({
+        uid: user.uid,
+        displayName: user.displayName,
+        email: user.email,
+        createdAt: admin.firestore.FieldValue.serverTimestamp(),
+    })
+    .then(() => {
+        console.log(`User ${user.uid} added to Firestore`);
+        return null;
+    })
+    .catch(error => {
+        console.error('Error adding user to Firestore', error);
+        return null;
+    });
+});
