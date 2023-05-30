a8c8ac51dbc47762f3a9e7ed5a4c0cfb2c477062
Danny McAteer <mcateerd2@gmail.com>
2023-05-30 01:22:37 +0000

    Remove admin lib from firebase file

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,7 +2,6 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
-import 'firebase/compat/admin';
 
 // Replace with your Firebase configuration object
 const firebaseConfig = {
