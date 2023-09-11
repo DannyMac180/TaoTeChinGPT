cf2b75d36c3646765fe580bfe6f6010b0973c77b
dan-mcateer <daniel.mcateer@udemy.com>
2023-09-11 15:02:30 -0400

    Move processStream() func to outside loop

--- pages/index.tsx
+++ pages/index.tsx
@@ -65,9 +65,9 @@ export default function TaoTeChing() {
             }
           }
         }
-        await processStream();
       }
     };
+    processStream();
   };
 
   const handleSubmit = async (e: { preventDefault: () => void }) => {
@@ -114,4 +114,3 @@ export default function TaoTeChing() {
     </div >
   );
 };
-
