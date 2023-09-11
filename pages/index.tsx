6d3532e3e9c915fb1cdc0d0896691f10424dc661
dan-mcateer <daniel.mcateer@udemy.com>
2023-09-11 15:46:08 -0400

    Check for 'undefined' at end of response and replace

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,11 +59,13 @@ export default function TaoTeChing() {
             const potentialJson = chunk.substring(5);
             try {
               const data = JSON.parse(potentialJson); // Remove the "data: " prefix
-              if (data === 'undefined') {
-                break
-              }
-              setResponseData(prevData => prevData + data.choices[0].delta.content);
-              console.log(responseData);
+              setResponseData(prevData => {
+                let newData = prevData + data.choices[0].delta.content;
+                if (newData.endsWith('undefined')) {
+                  newData = newData.replace(/undefined$/, '');
+                }
+                return newData;
+              });
             } catch (e) {
               console.error('Invalid JSON:', potentialJson);
             }
